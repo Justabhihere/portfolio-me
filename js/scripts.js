ea588a46fd813c71f1c3451dff6516cf3e8a4da2
Abhishek B <127709234+Justabhihere@users.noreply.github.com>
2023-12-23 23:14:29 +0530

    best ui

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// typing animation//
+var typed=new Typed(".typing",{
+    strings:[" ","Web Designer","Fornt-End Developer","UI/UX Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
