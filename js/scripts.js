d806f7c4ec5ff24f88a463c73a235008e5c3f1e0
Abhishek B <127709234+Justabhihere@users.noreply.github.com>
2024-08-17 21:09:19 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // typing animation//
 var typed=new Typed(".typing",{
-    strings:[" ","Web Designer","Fornt-End Developer","UI/UX Developer"],
+    strings:[" ","Web Designer","Full-Stack Developer","UI/UX Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -27,22 +27,21 @@ function setActiveStyle(color)
     })
 }
 // theme style light and dark mode//
-const dayNight =document.querySelector(".day-night");
-dayNight.addEventListener("click",() =>{
+const dayNight = document.querySelector(".day-night");
+
+// Set default theme to dark
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("dark");
+    dayNight.querySelector("i").classList.add("fa-sun");
+});
+
+dayNight.addEventListener("click", () => {
+    // Toggle the dark mode
+    document.body.classList.toggle("dark");
+    // Toggle the icon
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-})
-window.addEventListener("load",()=>{
-    if(document.body.classList.contains("dark"))
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");
+});
 
-    }
-})
 
 
