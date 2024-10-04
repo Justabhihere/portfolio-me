b2503ad728e39c81185a77db6bee597a94f40d30
Abhishek B <127709234+Justabhihere@users.noreply.github.com>
2024-10-04 19:14:08 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // typing animation//
 var typed=new Typed(".typing",{
-    strings:[" ","Web Designer","Full-Stack Developer","UI/UX Developer"],
+    strings:[" ","Web Designer","Full-Stack Developer","UI/UX Developer","Machine Learning Enthusiast"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
